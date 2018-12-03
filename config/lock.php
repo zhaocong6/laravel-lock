65e7229f26c03c91f661bd06b72c42a13565add5
zhaocong <1140253608@qq.com>
2018-12-03 17:15:29 +0800

    修改配置文件

--- config/lock.php
+++ config/lock.php
@@ -5,10 +5,13 @@ return [
     'drive' =>  'redis',
 
     'redis' =>  [
-        'host'                  =>  '127.0.0.1',
-        'port'                  =>  6379,
-        'read_write_timeout'    =>  0,
-        'persistent'            =>  true,
+        'host' => env('REDIS_HOST', '127.0.0.1'),
+        'port' => env('REDIS_PORT', 6379),
+        'read_write_timeout' => 0,
+        'persistent' => true,
+        'parameters'            =>[
+            'password'=>env('REDIS_PASSWORD', null)
+        ],
     ],
 
     'params' => [
